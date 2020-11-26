a1b50768aeaf50ce11c98c3ce603e583e3cf7496
hanawa_riki <riki012345678961@gmail.com>
2020-11-26 11:42:17 +0900

    VisualStudioでGitを設定

--- Git_Test_Project1/Git_Test_Project1.cpp
+++ Git_Test_Project1/Git_Test_Project1.cpp
@@ -1,11 +1,10 @@
 ﻿// Git_Test_Project1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello Hello\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
